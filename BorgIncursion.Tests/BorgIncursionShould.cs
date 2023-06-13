8f470e2741d6eade00cba90fe91a9b65d66839aa
Pere Frontera <pere.frontera@gmail.com>
2023-06-13 09:29:25 +0200

    Upload library & unit-tests

---
+++ BorgIncursion.Tests/BorgIncursionShould.cs
@@ -0,0 +1,68 @@
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace BorgIncursion.Tests;
+
+public class BorgIncursionShould : IDisposable
+{
+    private readonly object _droneBorg;
+    private readonly Type _assimilatedType;
+
+    public BorgIncursionShould()
+    {
+        _assimilatedType = BorgIncursion.Assimilate("BorgIncursion.Locutus", out _droneBorg);
+    }
+    
+    public void Dispose() => GC.SuppressFinalize(this);
+
+    [Fact]
+    public void Assimilate_and_return_new_instance_as_borg_drone()
+    {
+        var instance = _droneBorg;
+        
+        using (new AssertionScope())
+        {
+            instance.Should().NotBeNull();
+            instance.GetType().Name.Should().Be("Locutus");
+        }
+    }
+    
+    [Fact(Skip = "Working on it...")]
+    
+    public void Assimilate_and_return_new_instance_as_borg_drone_passing_params_to_the_ctor()
+    {
+        var parameters = new object[]{new {Message = "Resistance is futile!"}} ;
+        BorgIncursion.Assimilate("BorgIncursion.Locutus", 
+            out var instance,parameters);
+        
+        using (new AssertionScope())
+        {
+            instance.Should().NotBeNull();
+            instance.GetType().Name.Should().Be("Locutus");
+        }
+    }
+    
+    [Fact]
+    public void CollectMethod_and_returns_it_if_exists()
+    {
+        var method = _assimilatedType.CollectMethod("Add");
+
+        using (new AssertionScope())
+        {
+            method.Should().NotBeNull();
+            method!.Name.Should().Be("Add");
+        }
+    }
+
+    [Fact]
+    public void Invoke_method_from_assimilated_borg_drone_and_returns_result()
+    {
+        var method = _assimilatedType.CollectMethod("Add");
+    
+        var result = _droneBorg.NeuralInvokeAs<int>(method, 2, 3);
+    
+        result.Should().Be(5);
+    }
+}
+
+
---
+++ BorgIncursion/BorgIncursion.cs
@@ -0,0 +1,69 @@
+﻿using System.Reflection;
+
+namespace BorgIncursion;
+
+public static class BorgIncursion
+{
+    /// <summary>
+    /// Resistance is futile! Extracts an internal class and returns the Type. Also provides an instance of the class.
+    /// </summary>
+    /// <param name="className">The fully qualified name of the inner class.
+    /// Example of a namespace `CNN.Cancel.Operations.CancelOperation`</param>
+    /// <param name="instance">An instance of the extracted inner class.</param>
+    /// <param name="parameters">parameters to be passed to the constructor</param>
+    /// <returns>The Type object representing the extracted inner class.</returns>
+    public static Type Assimilate(string className, out object instance, params object[] parameters)
+    {
+        var callingAssembly = Assembly.GetCallingAssembly();
+        var targetAssembly = className.Split('.').FirstOrDefault();
+        var projectReference = callingAssembly.GetReferencedAssemblies()
+            .FirstOrDefault(a => a.Name == targetAssembly);
+        var outerAssembly = Assembly.LoadFrom($"{projectReference.Name}.dll");
+        var internalType = outerAssembly.GetType(className);
+
+        var constructor = internalType.GetConstructor(Type.EmptyTypes);
+        instance = constructor.Invoke(IsNullParameters(parameters) ? null : parameters);
+        return internalType;
+    }
+
+    private static bool IsNullParameters(object[] parameters)
+    {
+        return parameters is object[] && parameters.Length == 0;
+    }
+
+    /// <summary>
+    /// Invokes a method using reflection and returns the result as the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type to which the invocation result will be converted.</typeparam>
+    /// <param name="instance">The instance on which to invoke the method.</param>
+    /// <param name="method">The method information to invoke.</param>
+    /// <param name="parameters">The parameters for the method invocation.</param>
+    /// <returns>The result of the Cthulhu's invocation converted to the specified type.</returns>
+    public static T NeuralInvokeAs<T>(this object instance, MethodInfo method, params object[] parameters) =>
+        (T)method!.Invoke(instance, parameters)!;
+    
+    /// <summary>
+    /// Asks to the borg sphere for a method with the specified name on the given type.
+    /// </summary>
+    /// <param name="type">The type on which to search for the method.</param>
+    /// <param name="methodName">The name of the method to search for.</param>
+    /// <returns>The MethodInfo object representing the method, or null if not found.</returns>
+    public static MethodInfo? CollectMethod(this Type type, string methodName) =>
+        type!.GetMethod(methodName, BindingFlags.Instance | BindingFlags.Public);
+}
+
+internal class Locutus
+{
+    private string _message;
+    
+    public Locutus(){}
+    
+    public Locutus(string message)
+    {
+        _message = message;
+    }
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
